20c0a0ee1e6f87667bef0edba705cb3b3dd743b9
김민수 <112928989+Minsu0207@users.noreply.github.com>
2023-04-09 16:18:51 +0900

    [level 0] Title: 짝수의 합, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
@@ -0,0 +1,16 @@
+function solution(n) {
+    let result = 0;
+    if(n%2==0){
+        for(let i=2; i<=n ; i+=2){
+            result = i +result
+        }
+        return result;
+    }
+    else{
+        for(let i=2; i<=n ; i+=2){
+            result = i +result
+        }
+        return result;
+    }
+        
+}
